524f97b0096daf41bba51354f0c1fda543479851
Reyanii <indriyaniv9@gmail.com>
2025-03-17 16:31:48 +0800

    menambah kan For (pengulangan)

--- ConrohLooping.cpp
+++ ConrohLooping.cpp
@@ -2,3 +2,18 @@
 #include <ctime>
 #include <cstdlib>
 using namespace std;
+
+int main ()
+{
+    int n;
+    int perulanganWhile = 0;
+    int perulanganDo = 0;
+
+    // Pengulangan dengan for
+    
+    cout << "Perulangan for pencacah naik" << endl;
+    for (n = 0; n < 5; n++)
+    { // You, 5 hours ago * add all
+        cout << "Nilai n = " << 0 << " Selamat Datang " << endl;
+    }
+}
